2c740567db122af261328d514832617695e6e4a2
Zachary Breit <zbreit18@gmail.com>
2019-03-27 23:55:36 -0400

    Added the new tilted banners

--- assets/imgs/index.js
+++ assets/imgs/index.js
@@ -1,16 +1,16 @@
 // Use this file to staticly import images https://stackoverflow.com/questions/33907218/react-native-use-variable-for-image-file
 
 const Images = {
-    banner_food: require('./food_banner.png'),
-    banner_demo: require('./demo_banner.png'),
-    banner_main: require('./main-event_banner.png'),
-    banner_campfire: require('./campfire_banner.png'),
-    banner_sponsor: require('./sponsor_banner.png'),
-    banner_mentor: require('./mentor_banner.png'),
-    banner_mini: require('./mini-event_banner.png'),
-    banner_workshop: require('./workshop_banner.png'),
-    banner_ceremony: require('./ceremony_banner.png'),
-    banner_colorwar: require('./colorwar_banner.png'),
+    banner_food: require('./tilted-banners/food-tilt.jpg'),
+    banner_demo: require('./tilted-banners/demo-tilt.jpg'),
+    banner_main: require('./tilted-banners/main-tilt.jpg'),
+    banner_campfire: require('./tilted-banners/campfire-tilt.jpg'),
+    banner_sponsor: require('./tilted-banners/sponsor-tilt.jpg'),
+    banner_mentor: require('./tilted-banners/mentors-tilt.jpg'),
+    banner_mini: require('./tilted-banners/minievent-tilt.jpg'),
+    banner_workshop: require('./tilted-banners/workshop-tilt.jpg'),
+    banner_ceremony: require('./tilted-banners/ceremony-tilt.jpg'),
+    banner_colorwar: require('./tilted-banners/colorwar-tilt.jpg'),
 
     bitcamp_logo: require('./bitcamp-logo-icon.png')
 };
