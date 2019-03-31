4253d7e61391e02a784587e091cb2e2bd9819b48
Zachary Breit <zbreit18@gmail.com>
2019-03-30 23:25:01 -0400

    Sizing test with pink background modal headers

--- src/components/SearchModal.js
+++ src/components/SearchModal.js
@@ -150,6 +150,7 @@ export default class SearchModal extends Component {
               alignItems: 'center', 
               paddingHorizontal: scale(15),
               paddingTop: Platform.OS === "ios" ? getStatusBarHeight() : 0,
+              backgroundColor: 'pink', // TODO: remove background color
             }} 
             onLayout={(event) => this.measureView(event, 'SearchBar')}
           >
